13b3d2cc314a8ba93c105e709490573499bfa039
Fran Lucena <francisco.lucena@mobivery.com>
2023-08-17 22:42:52 +0200

    Initial Commit

---
+++ FlightConnections/FlightConnectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlightConnectionsApp.swift
+//  FlightConnections
+//
+//  Created by Francisco Lucena on 17/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FlightConnectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
